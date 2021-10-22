65d00163cd90278b7175b75e4ca0dd8c063dd87a
sharma-dushyant <dushyantsharma9594@gmail.com>
2021-10-22 19:19:17 +0530

    first commit

---
+++ lib/screens/auth/components/sign_in_form.dart
@@ -0,0 +1,50 @@
+// ignore_for_file: unused_field
+
+import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+
+import '../../../constants.dart';
+import 'sign_up_form.dart';
+
+// ignore: must_be_immutable
+class SignInForm extends StatelessWidget {
+  SignInForm({
+    Key? key,
+    required this.formKey,
+  }) : super(key: key);
+ 
+
+  final GlobalKey formKey;
+ 
+  late String _email, _password;
+   
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFieldName(text: "Email"),
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(hintText: "test@email.com"),
+            validator: EmailValidator(errorText: "Use a valid email!"),
+            onSaved: (email) => _email = email!,
+          ),
+          const SizedBox(height: defaultPadding),
+          TextFieldName(text: "Password"),
+          TextFormField(
+            // We want to hide our password
+            obscureText: true,
+            decoration: InputDecoration(hintText: "******"),
+            validator: passwordValidator,
+            onSaved: (password) => _password = password!,
+          ),
+          const SizedBox(height: defaultPadding),
+        ],
+      ),
+    );
+  }
+}
